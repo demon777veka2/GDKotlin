10c9f440dec176007c380bb69097299f55a0caeb
demon777veka2 <demon777veka@gmail.com>
2020-10-07 17:57:30 +0700

    #1 6. Добавлены комментарии к коду [x]

--- src/First.kt
+++ src/First.kt
@@ -4,7 +4,9 @@ fun main() {
     //Работа с текстом(задание 6)
     val text_mass: Array<String> = readLine().toString().split(" ").toTypedArray()
 
+    //Проверка  на содержание в тексте 1 слова "echo"
     if ("echo" == text_mass[0]) {
+        //удаление лишних символов из слов массива
         text_mass[1] = text_mass[1].substring(1)
         val simvol: String = text_mass[text_mass.size - 1]
         text_mass[text_mass.size - 1] = simvol.substring(0, simvol.length - 1)
@@ -14,6 +16,7 @@ fun main() {
     println("Задание 1")
     println("Результат:")
 
+    //Вывод значений массива
     for (i in 1..text_mass.size - 1) {
         println(text_mass[i])
     }
@@ -22,13 +25,15 @@ fun main() {
     println("Задание 2")
     println("Результат:")
 
+    //Пустая переменная массива для редактирования
     var time_mass_word: Array<String> = emptyArray<String>()
+    //Заполнение массива time_mass_word из массива text_mass с 1 элемента
     for (i in 1..text_mass.size - 1) {
         time_mass_word += text_mass[i]
     }
-
+    //Сортировка массива
     time_mass_word.sort()
-
+    //Вывод значений массива
     for (f in 0..time_mass_word.size - 1) {
         println(time_mass_word[f])
     }
@@ -37,14 +42,15 @@ fun main() {
     println("Задание 3")
     println("Результат:")
 
+    //Пустая переменная массива для редактирования
     time_mass_word = emptyArray<String>()
-
+    //Заполнение массива time_mass_word из массива text_mass с 1 элемента
     for (i in 1..text_mass.size - 1) {
         time_mass_word += text_mass[i]
     }
-
+    //Сортировка массива
     time_mass_word.sort()
-
+    //Перебор элементов 1 массива для создания уникальности массива
     for (f in 0..time_mass_word.size - 1) {
         for (g in f + 1..time_mass_word.size - 1) {
             if (time_mass_word[f] == time_mass_word[g]) {
@@ -52,7 +58,7 @@ fun main() {
             }
         }
     }
-
+    //Вывод значений массива
     for (h in 0..time_mass_word.size - 1) {
         if (time_mass_word[h] != "") {
             println(time_mass_word[h])
@@ -63,27 +69,31 @@ fun main() {
     println("Задание 4")
     println("Результат:")
 
+    //Пустая переменная массива для редактирования
     time_mass_word = emptyArray<String>()
-
+    //Заполнение массива time_mass_word из массива text_mass с 1 элемента
     for (i in 1..text_mass.size - 1) {
         time_mass_word += text_mass[i] + " 1"
     }
-
+    //Сортировка массива
     time_mass_word.sort()
-
+    //Перебор элементов 1 массива для создания уникальности массива и подсчета количества повторпений
     for (f in 0..time_mass_word.size - 1) {
+        //Переменная колличества повторений
         var koll_coincidence: Int = 1
         for (g in f + 1..time_mass_word.size - 1) {
+            //проверка на совпадение с подсчетом количества совпадений
             if (time_mass_word[f] == time_mass_word[g]) {
                 time_mass_word[g] = ""
                 koll_coincidence++
             }
         }
+        //Присвоение элементу массива колл. совпадений, если он не пустой
         if (time_mass_word[f] != "") {
             time_mass_word[f] = time_mass_word[f].substring(0, time_mass_word[f].length - 1) + koll_coincidence;
         }
     }
-
+    //Вывод значений массива
     for (h in 0..time_mass_word.size - 1) {
         if (time_mass_word[h] != "") {
             println(time_mass_word[h])
@@ -94,69 +104,76 @@ fun main() {
     println("Задание 5")
     println("Результат:")
 
+    //Переменная максимального значения совпадений
     var max_koll_coincidence = 1
-
+    //Заполнение массива time_mass_word из массива text_mass с 1 элемента
     for (i in 1 until text_mass.size) {
         time_mass_word[i - 1] = text_mass[i] + " 1"
     }
-
+    //Сортировка массива
     Arrays.sort(time_mass_word)
-
+    //Перебор элементов 1 массива для создания уникальности массива и подсчета количества повторпений
     for (f in time_mass_word.indices) {
-        val t1 = time_mass_word[f].toCharArray()
+        //Цифра повторения слова в массиве
+        val length_word = time_mass_word[f].toCharArray()
         var koll_coincidence = 1
-
         for (g in f + 1 until time_mass_word.size) {
-            val t2 = time_mass_word[g].toCharArray()
-
-            if (t1.size == t2.size) {
-                var qer = 0
-                for (ss in t1.indices) {
-                    if (t1[ss] == t2[ss]) {
-                        qer++
+            //Цифра повторения слова в массиве
+            val length_word2 = time_mass_word[g].toCharArray()
+            //Проверка длины
+            if (length_word.size == length_word2.size) {
+                //переменная количества элементов совпадающих с другим элементом слова
+                var koll_element = 0
+                //Цыкл совпадений элементов
+                for (ss in length_word.indices) {
+                    if (length_word[ss] == length_word2[ss]) {
+                        koll_element++
                     }
                 }
-
-                if (qer == t1.size) {
+                //Проверка на совпадения колличества одинаковых элементов в разных переменных
+                if (koll_element == length_word.size) {
                     time_mass_word[g] = ""
                     koll_coincidence++
-
+                        //Проверка на самое большое число повторений слова в массиве
                     if (max_koll_coincidence < koll_coincidence) {
                         max_koll_coincidence = koll_coincidence
                     }
                 }
             }
         }
-
+        //Присвоение элементу массива колл. совпадений, если он не пустой
         if (time_mass_word[f] !== "") {
             time_mass_word[f] = time_mass_word[f].substring(0, time_mass_word[f].length - 1) + koll_coincidence
         }
     }
-
+        //Цикл с максисального значения совпадений до 1, с сортировкой по цифре и алфавиту
     for (k in max_koll_coincidence downTo 1) {
+        //Временный массив с элементами
         val list_sort_word = ArrayList<String>()
         list_sort_word.clear()
         var koll_coincidence = 0
-
+        //Перебор элементов для нахождения нужного элемента конечное значение, которого равно значению цикла с переменной k
         for (l in time_mass_word.indices) {
             val time_word = time_mass_word[l].split("".toRegex()).dropLastWhile { it.isEmpty() }
-
+            //Проверка на пустоту элемента
             if (time_word.size != 0 && time_word[time_word.size - 1] !== "") {
                 val cifr_reapet = Integer.valueOf(time_word[time_word.size - 1])
-
+                //проверка на совпадения колличеста совпадений слова со значением переменной k
                 if (k === cifr_reapet) {
                     koll_coincidence++
                     list_sort_word.add(time_mass_word[l])
                 }
             }
         }
-
+        //Проверка на колличества элементов попавшие под условия "k === cifr_reapet"
         if (koll_coincidence > 1) {
+            //Сортировка и вывод элементов
             Collections.sort(list_sort_word)
             for (word in list_sort_word) {
                 println(word)
             }
         } else if (koll_coincidence == 1) {
+            //Вывод элементов
             for (word in list_sort_word) {
                 println(word)
             }
